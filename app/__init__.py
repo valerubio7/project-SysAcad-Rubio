e5c3012449c8ab0243dbb7dceb31d2b9aa7e24c9
Pablo Geyer <geyerpablo2@gmail.com>
2025-05-28 12:35:08 -0300

    refactor: mejorar estructura del archivo __init__.py en la carpeta app

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,24 @@
 import logging
 from flask import Flask
-from flask_apispec import FlaskApiSpec
-from flask_marshmallow import Marshmallow
-from flask_caching import Cache
 import os
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from app.route import RouteApp
 from app.config import config
 
 db = SQLAlchemy()
-migrate = Migrate()
-
-
+migrate=Migrate()
 def create_app() -> Flask:
     """
     Using an Application Factory
     Ref: Book Flask Web Development Page 78
     """
     app_context = os.getenv('FLASK_CONTEXT')
-    #https://flask.palletsprojects.com/en/3.0.x/api/#flask.Flask
+    #https://flask.palletsprojects.com/en/stable/api/#flask.Flask
     app = Flask(__name__)
     f = config.factory(app_context if app_context else 'development')
     app.config.from_object(f)
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app,db)
 
     @app.shell_context_processor    
     def ctx():
